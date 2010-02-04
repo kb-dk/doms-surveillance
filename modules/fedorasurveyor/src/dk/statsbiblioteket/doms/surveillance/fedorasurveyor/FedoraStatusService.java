781de5dc296ef7b6f72bb95f4d1d6bb24fb00c7c
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2010-02-04 09:21:16 +0000

    Review followup. Mostly documentation and codestyle. Fixes two calls to Fedora instead of one, and better HTML formatting at end

--- modules/fedorasurveyor/src/dk/statsbiblioteket/doms/surveillance/fedorasurveyor/FedoraStatusService.java
+++ modules/fedorasurveyor/src/dk/statsbiblioteket/doms/surveillance/fedorasurveyor/FedoraStatusService.java
@@ -45,12 +45,13 @@ import javax.ws.rs.core.Context;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-/** Class that exposes log messages as surveyable messages over REST. */
+/** Class that exposes fedora status as surveyable messages over REST. */
 @QAInfo(author = "kfc",
         reviewers = "jrg",
         level = QAInfo.Level.NORMAL,
-        state = QAInfo.State.QA_NEEDED)
+        state = QAInfo.State.QA_OK)
 @Path("/")
 public class FedoraStatusService implements Surveyable {
     /** The application name for what is being surveyed. */
@@ -59,21 +60,25 @@ public class FedoraStatusService implements Surveyable {
     /** Logger for this class. */
     private Log log = LogFactory.getLog(getClass());
 
-    /** The web service context. Injected by the web service. */
+    /** The web service context. Injected by the web service framework. */
     @Context
     private ServletConfig config;
 
+    /** Prefix for parameter names. */
+    private static final String PARAMETER_PACKAGENAME_PREFIX
+            = "dk.statsbiblioteket.doms.surveyor.fedorasurveyor";
+
     /** Parameter name for Fedora URL. */
     private static final String FEDORA_URL_PARAMETER
-            = "dk.statsbiblioteket.doms.surveyor.fedorasurveyor.fedoraUrl";
+            = PARAMETER_PACKAGENAME_PREFIX + ".fedoraUrl";
 
     /** Parameter name for Fedora user name. */
     private static final String FEDORA_USER_PARAMETER
-            = "dk.statsbiblioteket.doms.surveyor.fedorasurveyor.fedoraUser";
+            = PARAMETER_PACKAGENAME_PREFIX + ".fedoraUser";
 
     /** Parameter name for Fedora password. */
     private static final String FEDORA_PASSWORD_PARAMETER
-            = "dk.statsbiblioteket.doms.surveyor.fedorasurveyor.fedoraPassword";
+            = PARAMETER_PACKAGENAME_PREFIX + ".fedoraPassword";
 
     /** Read parameter for Fedora URL. */
     private String fedoraUrl;
@@ -84,22 +89,38 @@ public class FedoraStatusService implements Surveyable {
     /** Read parameter for Fedora password. */
     private String fedoraPassword;
 
+    /**
+     * Behaves exactly like getStatus().
+     *
+     * @param time Ignored.
+     * @return A realtime status of Fedora.
+     * @see #getStatus
+     */
     @GET
     @Path("getStatusSince/{date}")
     @Produces("application/xml")
-    /** Returns the current status of Fedora. On trouble communicating with
+    public Status getStatusSince(@PathParam("date") long time) {
+        log.trace("Enter getStatusSince(" + time + ")");
+        return getStatus();
+    }
+
+    /**
+     * Returns the current status of Fedora. On trouble communicating with
      * Fedora, a status reporting this is generated.
      *
      * This method acts as fault barrier for communication with Fedora.
      *
-     * @param time Only messages strictly after the given date are returned.
-     * @return A status containing list of log messages.
+     * @return A realtime status of Fedora.
      */
-    public Status getStatusSince(@PathParam("date") long time) {
-        log.trace("Enter getStatusSince(" + time + ")");
-        initialize();
-
+    @GET
+    @Path("getStatus")
+    @Produces("application/xml")
+    public Status getStatus() {
+        log.trace("Enter getStatus()");
+        List<StatusMessage> list = new ArrayList<StatusMessage>();
         StatusMessage statusMessage;
+
+        initialize();
         try {
             statusMessage = getFedoraStatus();
         } catch (Exception e) {
@@ -109,37 +130,27 @@ public class FedoraStatusService implements Surveyable {
                     StatusMessage.Severity.RED, System.currentTimeMillis(),
                     false);
         }
-        ArrayList<StatusMessage> list = new ArrayList<StatusMessage>();
         list.add(statusMessage);
-        return new Status(APPLICATION_NAME, list);
-    }
 
-    @GET
-    @Path("getStatus")
-    @Produces("application/xml")
-    /** Returns all log messages received.
-     *
-     * @return A status containing list of log messages.
-     */
-    public Status getStatus() {
-        log.trace("Enter getStatus()");
-        return getStatusSince(0L);
+        return new Status(APPLICATION_NAME, list);
     }
 
     /** Initialise the web service by reading the parameters. */
     private synchronized void initialize() {
+        log.trace("Enter initialize()");
         String fedoraUrl = config.getInitParameter(FEDORA_URL_PARAMETER);
+        String fedoraUser = config.getInitParameter(FEDORA_USER_PARAMETER);
+        String fedoraPassword = config
+                .getInitParameter(FEDORA_PASSWORD_PARAMETER);
+
         if (!this.fedoraUrl.equals(fedoraUrl)) {
             this.fedoraUrl = fedoraUrl;
             log.info("Setting parameter fedoraUrl to '" + fedoraUrl + "'");
         }
-        String fedoraUser = config.getInitParameter(FEDORA_USER_PARAMETER);
         if (!this.fedoraUser.equals(fedoraUser)) {
             this.fedoraUser = fedoraUser;
             log.info("Setting parameter fedoraUser to '" + fedoraUser + "'");
         }
-        String fedoraPassword = config
-                .getInitParameter(FEDORA_PASSWORD_PARAMETER);
         if (!this.fedoraPassword.equals(fedoraPassword)) {
             this.fedoraPassword = fedoraPassword;
             log.info("Setting parameter fedoraPassword to '" + fedoraPassword
@@ -157,13 +168,15 @@ public class FedoraStatusService implements Surveyable {
      * @throws Exception On any trouble communicating with Fedora.
      */
     private StatusMessage getFedoraStatus() throws Exception {
+        log.trace("Enter getFedoraStatus()");
         FedoraClient fedoraClient = new FedoraClient(fedoraUrl, fedoraUser,
                                                      fedoraPassword);
-        fedoraClient.getAPIA().describeRepository();
         RepositoryInfo description = fedoraClient.getAPIA()
                 .describeRepository();
+
         fedoraClient.getAPIA()
                 .getObjectProfile(description.getSamplePID(), null);
+        //Done in order to provoke exception on trouble
         new URL(description.getSampleSearchURL()).openConnection();
         return new StatusMessage(descriptionToStatus(description),
                                  StatusMessage.Severity.GREEN,
@@ -173,34 +186,44 @@ public class FedoraStatusService implements Surveyable {
     /**
      * Converts a repository description to a string.
      *
-     * @param description The repository description
+     * @param description The repository description. Should never be null.
      * @return A string with all information from the repository description.
+     * This is formatted in HTML.
+     * @throws IllegalArgumentException On null parameter
      */
     private String descriptionToStatus(RepositoryInfo description) {
+        log.trace("Enter descriptionToStatus(...)");
         if (description == null) {
             throw new IllegalArgumentException(
                     "Parameter 'RepositoryInfo description'"
                             + " should not be null");
         }
         return "AdminEmailList: '"
-                + Arrays.toString(description.getAdminEmailList()) + "'<br>\n"
+                + Arrays.toString(description.getAdminEmailList()) + "'<br />\n"
                 + "DefaultExportFormat: '"
-                + description.getDefaultExportFormat() + "'<br>\n"
-                + "OAINamespace: '" + description.getOAINamespace() + "'<br>\n"
-                + "RepositoryBaseURL: '" + description.getRepositoryBaseURL()
-                + "'<br>\n" + "RepositoryName: '"
-                + description.getRepositoryName() + "'<br>\n"
+                + description.getDefaultExportFormat() + "'<br />\n"
+                + "OAINamespace: '"
+                + description.getOAINamespace() + "'<br />\n"
+                + "RepositoryBaseURL: '"
+                + description.getRepositoryBaseURL() + "'<br />\n"
+                + "RepositoryName: '"
+                + description.getRepositoryName() + "'<br />\n"
                 + "RepositoryPIDNamespace: '"
-                + description.getRepositoryPIDNamespace() + "'<br>\n"
-                + "RepositoryVersion: '" + description.getRepositoryVersion()
-                + "'<br>\n" + "RetainPIDs: '"
-                + Arrays.toString(description.getRetainPIDs()) + "'<br>\n"
-                + "SampleAccessURL: '" + description.getSampleAccessURL()
-                + "'<br>\n" + "SampleOAIIdentifier: '"
-                + description.getSampleOAIIdentifier() + "'<br>\n"
-                + "SampleOAIURL: '" + description.getSampleOAIURL() + "'<br>\n"
-                + "SamplePID: '" + description.getSamplePID() + "'<br>\n"
-                + "AdminEmailList: '" + description.getSampleSearchURL() + "'";
+                + description.getRepositoryPIDNamespace() + "'<br />\n"
+                + "RepositoryVersion: '"
+                + description.getRepositoryVersion() + "'<br />\n"
+                + "RetainPIDs: '"
+                + Arrays.toString(description.getRetainPIDs()) + "'<br />\n"
+                + "SampleAccessURL: '"
+                + description.getSampleAccessURL() + "'<br />\n"
+                + "SampleOAIIdentifier: '"
+                + description.getSampleOAIIdentifier() + "'<br />\n"
+                + "SampleOAIURL: '"
+                + description.getSampleOAIURL() + "'<br />\n"
+                + "SamplePID: '"
+                + description.getSamplePID() + "'<br />\n"
+                + "SampleSearchURL: '" 
+                + description.getSampleSearchURL() + "'";
     }
 
 }
